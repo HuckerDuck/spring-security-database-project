69f7593b9b9974b97a6afc9e596e3ed4fb2f4e76
Fredrik <frmb1@hotmail.com>
2025-06-02 11:37:36 +0200

    Initial Commit with depencies in Spring Added

---
+++ src/main/java/se/sti/fredrik/secureapp/SpringSecurityGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package se.sti.fredrik.secureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGroupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityGroupProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/sti/fredrik/secureapp/SpringSecurityGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sti.fredrik.secureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityGroupProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
