b91c849d3b3961e7cdabaeaf39b14c8ef4c50089
Fredrik <frmb1@hotmail.com>
2025-06-02 13:20:34 +0200

    Second commit, added information to the database and tested it

--- src/test/java/se/sti/fredrik/secureapp/SpringSecurityGroupProjectApplicationTests.java
+++ src/test/java/se/sti/fredrik/secureapp/SpringSecurityGroupProjectApplicationTests.java
@@ -1,13 +1,15 @@
 package se.sti.fredrik.secureapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringSecurityGroupProjectApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private Datasource datasource;
+
+
 
 }
