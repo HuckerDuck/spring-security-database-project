a212c9c9db965906bff1e51fbba3b2634c0857c2
dkslinkyyy <slinky.sthlm@gmail.com>
2025-06-03 13:34:31 +0200

    Add: final impl of TokenService & AuthController

--- src/main/java/se/sti/fredrik/secureapp/controlller/AuthController.java
+++ src/main/java/se/sti/fredrik/secureapp/controlller/AuthController.java
@@ -1,14 +1,27 @@
 package se.sti.fredrik.secureapp.controlller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("request-token")
+@RequestMapping("/request-token")
 public class AuthController {
-
+    private AuthenticationManager authenticationManager;
+    private se.sti.fredrik.secureapp.service.TokenService tokenService;
+    public record LoginRequest(String username, String password) {}
     @PostMapping
     public ResponseEntity<String> token(@RequestBody LoginRequest loginRequest) {
-        Respons
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.username(),
+                        loginRequest.password()
+                )
+        );
+
+        String token = tokenService.generateToken(auth);
+        return ResponseEntity.ok(token);
     }
 }
--- src/main/java/se/sti/fredrik/secureapp/service/TokenService.java
+++ src/main/java/se/sti/fredrik/secureapp/service/TokenService.java
@@ -1,7 +1,31 @@
 package se.sti.fredrik.secureapp.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import se.sti.fredrik.secureapp.Config.SecurityConfig;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+
 public class TokenService {
 
+    private JwtEncoder jwtEncoder;
+    public TokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
     public String generateToken(Authentication authentication) {
+        Instant now = Instant.now();
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+
+        JwtClaimsSet claims = JwtClaimsSet.builder().issuer("self").issuedAt(now).expiresAt(now.plus(1, ChronoUnit.HOURS)).claim("scope", scope).build();
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 }
