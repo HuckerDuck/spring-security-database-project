4e66839d8ffad78901fb50d730196b08c0497524
Fredrik <181095176+HuckerDuck@users.noreply.github.com>
2025-06-10 14:45:05 +0200

    Commit efter merge
    Tog bort 3 rader som var kvar efter merge

--- src/main/java/se/sti/fredrik/secureapp/exception/GlobalExceptionHandler.java
+++ src/main/java/se/sti/fredrik/secureapp/exception/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.security.authentication.BadCredentialsException;
-=======
+
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
