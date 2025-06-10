08809a8078503b9d4d9664e22d543f30d8bb5a36
Fredrik <181095176+HuckerDuck@users.noreply.github.com>
2025-06-10 11:24:29 +0200

    initial commit of a new branch for testing

---
+++ src/test/java/se/sti/fredrik/secureapp/controlller/AuthControllerTest.java
@@ -0,0 +1,12 @@
+package se.sti.fredrik.secureapp.controlller;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+
+
+@WebMvcTest(AuthController.class)
+class AuthControllerTest {
+
+
+}
