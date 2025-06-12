38c538a27ff5f6b545562fb9cd8f61119ab2282a
Mojujo <oscar.tornqvist01@gmail.com>
2025-06-12 14:05:41 +0200

    add: javadoc for TestLoginHelper

--- src/test/java/se/sti/fredrik/secureapp/controller/TestLoginHelper.java
+++ src/test/java/se/sti/fredrik/secureapp/controller/TestLoginHelper.java
@@ -7,8 +7,19 @@ import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+/**
+ * Utility class for handling authentication for tests
+ * Provides methods to format JSON and obtain access tokens via the login endpoint
+ */
 public class TestLoginHelper {
+
+    /**
+     * Builds a JSON string representing a login body request
+     *
+     * @param username the username to format in the login request
+     * @param password the password to format in the login request
+     * @return a JSON string with the correct formatting
+     */
     public static String buildLoginJson(String username, String password) {
         return """
                 {
@@ -18,6 +29,17 @@ public class TestLoginHelper {
                 """.formatted(username, password);
     }
 
+    /**
+     * Performs a login request at the authentication endpoint using MockMvc
+     * and obtains a Jwt access token from the response
+     * <p>Uses {@link #buildLoginJson} to format</p>
+     *
+     * @param username the username to authenticate with
+     * @param password the password to authenticate with
+     * @param mockMvc the MockMvc instance to perform the HTTP request
+     * @return the Jwt access token as a string
+     * @throws Exception if the login request fails
+     */
     public static String obtainAccessToken(String username, String password, MockMvc mockMvc) throws Exception {
         String login = buildLoginJson(username, password);
 
